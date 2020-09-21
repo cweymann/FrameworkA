d5c8ea914b3be38cd833abdda478bea1e27282da
Claus Weymann <c.weymann@avm.de>
2020-09-21 21:28:48 +0200

    init

---
+++ Example/Pods/Target Support Files/Pods-FrameworkA_Objc_DependencyIssue_Example/Pods-FrameworkA_Objc_DependencyIssue_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FrameworkA_Objc_DependencyIssue_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FrameworkA_Objc_DependencyIssue_Example
+@end
